ad440484c3d6864c5dc90f805709af6d0c545e38
JerryGu-gjw <Jerrygu.gjw@gmail.com>
2021-12-22 12:44:24 +0800

    beautied

--- libraries/sonars/Example/simleUseSonar.ino
+++ libraries/sonars/Example/simleUseSonar.ino
@@ -3,13 +3,13 @@
 int distanceOfSonar1;
 Sonar sonar1;
 
-void setup(){
-    sonar1.setSonarPin(8, 9);                   //trigPin,echoPin
-    Serial.begin(9600);
+void setup() {
+  sonar1.setSonarPin(8, 9);                   //trigPin,echoPin
+  Serial.begin(9600);
 }
 
-void loop(){
-    distanceOfSonar1 = sonar1.sonarStart();    
-    Serial.print("Distance1: ");
-    Serial.println(distanceOfSonar1);
+void loop() {
+  distanceOfSonar1 = sonar1.sonarStart();
+  Serial.print("Distance1: ");
+  Serial.println(distanceOfSonar1);
 }
